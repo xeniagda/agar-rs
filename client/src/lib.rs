7ab5b47e5ce0cf5f42561d7311377adc4dd1b197
loovjo <jonathan.loov@gmail.com>
2018-06-25 16:50:45 +0200

    s/scale/zoom

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -25,7 +25,7 @@ const LINE_SPACE: f64 = 5.;
 lazy_static! {
     static ref SIZE: Mutex<(usize, usize)> = Mutex::new((0, 0));
     static ref STATE: Mutex<(State, usize)> = Mutex::new((State::new(), 0)); // State, client_id
-    static ref SCALE: Mutex<f64> = Mutex::new(1.);
+    static ref ZOOM: Mutex<f64> = Mutex::new(1.);
 
     //static ref LAST_TICK: Mutex<Option<Instant>> = Mutex::new(None);
 }
@@ -87,8 +87,8 @@ pub fn redraw() {
 
 #[wasm_bindgen]
 pub fn scroll(y: f64) {
-    if let Ok(mut scale) = SCALE.lock() {
-        *scale = (*scale - y / 20.).max(0.4).min(3.);
+    if let Ok(mut zoom) = ZOOM.lock() {
+        *zoom = (*zoom - y / 20.).max(0.4).min(3.);
     }
 }
 
@@ -109,36 +109,36 @@ fn draw() {
     if size.is_err() { return; }
     let size = size.unwrap();
 
-    let scale_mul = SCALE.lock();
-    if scale_mul.is_err() { return; }
-    let scale_mul = *scale_mul.unwrap();
+    let zoom_mul = ZOOM.lock();
+    if zoom_mul.is_err() { return; }
+    let zoom_mul = *zoom_mul.unwrap();
 
 
     if let Ok(state) = STATE.lock() {
         let my_pos = state.0.players.get(&state.1).map(|x| x.pos).unwrap_or((0., 0.));
         let my_size = state.0.players.get(&state.1).map(|x| x.show_size).unwrap_or(10.);
 
-        let scale = scale_mul / (my_size.sqrt() + 2.) * 30.;
+        let zoom = zoom_mul / (my_size.sqrt() + 2.) * 30.;
 
         // Grid lines
         let x_scroll = (my_pos.0 / LINE_SPACE - ((my_pos.0 / LINE_SPACE) as i64) as f64) * LINE_SPACE;
-        let lines = (size.0 as f64 / (scale * LINE_SPACE)) as i64 + 2;
+        let lines = (size.0 as f64 / (zoom * LINE_SPACE)) as i64 + 2;
         for x in -lines / 2..lines / 2 + 1 {
             let x = x as f64;
             put_line(
-                (x * scale * LINE_SPACE - x_scroll * scale + size.0 as f64 / 2., 0.),
-                (x * scale * LINE_SPACE - x_scroll * scale + size.0 as f64 / 2., size.1 as f64),
+                (x * zoom * LINE_SPACE - x_scroll * zoom + size.0 as f64 / 2., 0.),
+                (x * zoom * LINE_SPACE - x_scroll * zoom + size.0 as f64 / 2., size.1 as f64),
                 1.,
                 (200, 200, 200)
             );
         }
         let y_scroll = (my_pos.1 / LINE_SPACE - ((my_pos.1 / LINE_SPACE) as i64) as f64) * LINE_SPACE;
-        let lines = (size.1 as f64 / (scale * LINE_SPACE)) as i64 + 2;
+        let lines = (size.1 as f64 / (zoom * LINE_SPACE)) as i64 + 2;
         for y in -lines / 2..lines / 2 + 1 {
             let y = y as f64;
             put_line(
-                (           0., y * scale * LINE_SPACE - y_scroll * scale + size.1 as f64 / 2.),
-                (size.0 as f64, y * scale * LINE_SPACE - y_scroll * scale + size.1 as f64 / 2.),
+                (           0., y * zoom * LINE_SPACE - y_scroll * zoom + size.1 as f64 / 2.),
+                (size.0 as f64, y * zoom * LINE_SPACE - y_scroll * zoom + size.1 as f64 / 2.),
                 1.,
                 (200, 200, 200)
             );
@@ -146,9 +146,9 @@ fn draw() {
 
         for ball in &state.0.balls {
             put_circle(
-                ((ball.pos.0 - my_pos.0) * scale + size.0 as f64 / 2.,
-                 (ball.pos.1 - my_pos.1) * scale + size.1 as f64 / 2.),
-                scale,
+                ((ball.pos.0 - my_pos.0) * zoom + size.0 as f64 / 2.,
+                 (ball.pos.1 - my_pos.1) * zoom + size.1 as f64 / 2.),
+                zoom,
                 ball.color
             );
         }
@@ -157,9 +157,9 @@ fn draw() {
                 .sorted_by(|x, y| PartialOrd::partial_cmp(&x.size, &y.size).unwrap_or(Ordering::Less));
         for player in sorted_players {
             put_circle(
-                ((player.pos.0 - my_pos.0) * scale + size.0 as f64 / 2.,
-                 (player.pos.1 - my_pos.1) * scale + size.1 as f64 / 2.),
-                player.show_size * scale,
+                ((player.pos.0 - my_pos.0) * zoom + size.0 as f64 / 2.,
+                 (player.pos.1 - my_pos.1) * zoom + size.1 as f64 / 2.),
+                player.show_size * zoom,
                 (255, 0, 255)
             );
         }
